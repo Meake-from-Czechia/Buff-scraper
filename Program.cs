2053d1d4fb59c992ec78ecbec1a50b92e759d97a
Meake <meake@kekesh-laptop>
2023-04-06 13:00:06 +0200

    Revamped look of the found item dialogue, added name to the dialogue

--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ namespace vex_shit_v2
         }
         public static async Task GetSteamPrices(string activeLink)
         {
+            string name;
             double[] price = new double[2];
             var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
@@ -55,14 +56,17 @@ namespace vex_shit_v2
             {
                 const string steamPriceEval = @"Array.from(document.querySelectorAll('.steam-price span')).map(price => price.textContent);";
                 const string lowestPriceEval = @"Array.from(document.querySelectorAll('.buff')).map(price => price.textContent);";
+                const string nameEval = @"Array.from(document.querySelectorAll('.name')).map(name => name.textContent);";
                 string[] steamPrices = await page.EvaluateExpressionAsync<string[]>(steamPriceEval);
                 if (steamPrices.Length == 0) price[0] = 0; else price[0] = CleanUpPrice(steamPrices[0]);
                 string[] lowestPrices = await page.EvaluateExpressionAsync<string[]>(lowestPriceEval);
                 if (lowestPrices.Length == 0) price[1] = 999999; else price[1] = CleanUpPrice(lowestPrices[0]);
+                name = (await page.EvaluateExpressionAsync<string[]>(nameEval))[0];
                 await Console.Out.WriteAsync(". ");
-                if (price[1] / price[0] < 0.95 && price[0] - price[1] < 1)
+                if (price[1] / price[0] < 0.95 && price[0] - price[1] > 1)
                 {
-                    ColorWrite("\nNalezen předmět.\n", ConsoleColor.Green);
+                    ColorWrite("\n|=======-Nalezen předmět-=======|\n", ConsoleColor.Green);
+                    ColorWrite($"Jméno: {name}\n", ConsoleColor.Yellow);
                     Console.WriteLine($"Podíl: {price[1] / price[0]}");
                     Console.WriteLine($"Link: {activeLink}");
                 }  
