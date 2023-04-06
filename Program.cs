85cc97af7959e567a81d6656378be801ee3253a5
Meake <meake@kekesh-laptop>
2023-04-06 11:39:58 +0200

    Changed the app to close and open a new browser for every link, implemented a method for checking whether the browser is downloaded. Running it just once as opposed to checking on every instance. Default timeout is 30 seconds now.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using PuppeteerSharp;
 
 namespace vex_shit_v2
@@ -7,72 +8,71 @@ namespace vex_shit_v2
     {
         static void Main(string[] args)
         {
+            string activeLink;
+            
+            double[] price = new double[2];
             Random randNum = new Random();
             int[] randLinks = Enumerable
                 .Repeat(0, 10000)
                 .Select(i => randNum.Next(1, 20000))
                 .ToArray();
-
-            GetSteamPrices(randLinks).GetAwaiter().GetResult();
+            FetchBrowser().GetAwaiter().GetResult();
+            foreach (int i in randLinks)
+            {
+                Console.Write($"{i}: ");
+                activeLink = $"https://buff.market/market/goods/{i}?game=csgo";
+                GetSteamPrices(activeLink).GetAwaiter().GetResult();
+            }
         }
-        public static async Task GetSteamPrices(int[] randLinks)
+        public static async Task FetchBrowser()
         {
-            string activeLink;
-            double[] price = new double[2];
             await Console.Out.WriteAsync("Downloading browser... ");
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
             ColorWrite("Done. \n", ConsoleColor.Green);
+        }
+        public static async Task GetSteamPrices(string activeLink)
+        {
+            double[] price = new double[2];
             var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = true
             });
             var page = await browser.NewPageAsync();
             page.DefaultTimeout = 60000;
-            foreach (int url in randLinks)
+            await Console.Out.WriteAsync(".");
+            try
             {
-                Console.Write($"{url}: ");
-                activeLink = $"https://buff.market/market/goods/{url}?game=csgo";
+                await page.GoToAsync(activeLink);
                 await Console.Out.WriteAsync(".");
-                try
-                {
-                    await page.GoToAsync(activeLink);
-                    await Console.Out.WriteAsync(".");
-                    await page.WaitForNetworkIdleAsync();
-                    //await Task.Delay(5000);
-                }
-                catch (Exception ex)
-                {
-                    ColorWrite(ex.Message+ '\n', ConsoleColor.Red);
-                    continue;
-                }
-                try
-                {
-                    const string steamPriceEval = @"Array.from(document.querySelectorAll('.steam-price span')).map(price => price.textContent);";
-                    const string lowestPriceEval = @"Array.from(document.querySelectorAll('.buff')).map(price => price.textContent);";
-                    string[] steamPrices = await page.EvaluateExpressionAsync<string[]>(steamPriceEval);
-                    if (steamPrices.Length == 0) price[0] = 0; else price[0] = CleanUpPrice(steamPrices[0]);
-                    string[] lowestPrices = await page.EvaluateExpressionAsync<string[]>(lowestPriceEval);
-                    if (lowestPrices.Length == 0) price[1] = 999999; else price[1] = CleanUpPrice(lowestPrices[0]);
-                    await Console.Out.WriteAsync(". ");
-                    if (price[1] / price[0] < 0.95 && price[0] - price[1] < 1)
-                    {
-                        ColorWrite("\nNalezen předmět.\n", ConsoleColor.Green);
-                        Console.WriteLine($"Podíl: {price[1] / price[0]}");
-                        Console.WriteLine($"Link: {activeLink}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Podíl: {price[1] / price[0]}");
-                        Console.WriteLine($"Link: {activeLink}");
-                    }
-                }
-                catch 
-                { 
-                    ColorWrite(" Invalid prices.\n", ConsoleColor.Red);
-                    continue;
-                }
-                
+                await page.WaitForNetworkIdleAsync();
+                //await Task.Delay(5000);
             }
+            catch (Exception ex)
+            {
+                ColorWrite(ex.Message+ '\n', ConsoleColor.Red);
+            }
+            try
+            {
+                const string steamPriceEval = @"Array.from(document.querySelectorAll('.steam-price span')).map(price => price.textContent);";
+                const string lowestPriceEval = @"Array.from(document.querySelectorAll('.buff')).map(price => price.textContent);";
+                string[] steamPrices = await page.EvaluateExpressionAsync<string[]>(steamPriceEval);
+                if (steamPrices.Length == 0) price[0] = 0; else price[0] = CleanUpPrice(steamPrices[0]);
+                string[] lowestPrices = await page.EvaluateExpressionAsync<string[]>(lowestPriceEval);
+                if (lowestPrices.Length == 0) price[1] = 999999; else price[1] = CleanUpPrice(lowestPrices[0]);
+                await Console.Out.WriteAsync(". ");
+                if (price[1] / price[0] < 0.95 && price[0] - price[1] < 1)
+                {
+                    ColorWrite("\nNalezen předmět.\n", ConsoleColor.Green);
+                    Console.WriteLine($"Podíl: {price[1] / price[0]}");
+                    Console.WriteLine($"Link: {activeLink}");
+                }  
+            }
+            catch 
+            {
+
+                ColorWrite(" Invalid prices.\n", ConsoleColor.Red);
+            }
+            await browser.CloseAsync();
         }
         public static double CleanUpPrice(string price)
         {
